35e1bbaaf0044c86ab2c3bcbc5c2724d0be9d06c
Dmitry Prigozhaev <dmitry.prigozhaev@gmail.com>
2018-10-20 12:40:55 +0300

    Настроено взаимодействие с БД.

--- src/main/java/ru/tsystems/school/Main.java
+++ src/main/java/ru/tsystems/school/Main.java
@@ -1,7 +1,14 @@
 package ru.tsystems.school;
 
+import ru.tsystems.school.entities.User;
+import ru.tsystems.school.services.UserService;
+
 public class Main {
     public static void main(String[] args) {
 
+        UserService userService = new UserService();
+        User user = userService.readUser(1);
+        System.out.println(user);
+        userService.createUser(user);
     }
 }
